11c0c0294ceb5fb3eacd18acfccec727f3ade4a8
Jason Xu <guoxuan.xu8@gmail.com>
2026-02-28 16:19:42 -0800

    implement use selector that render the total quantity and items

--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './CartSummary.css';
 
 const CartSummary = () => {
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+  const totalPrice = useSelector((state) => state.cart.totalPrice);
+
+
   return (
     <div className="cart-summary">
-      <h3 className="cart-summary-text">Total Items: {0}</h3>
-      <h3 className="cart-summary-text">Total Price: {0}</h3>
+      <h3 className="cart-summary-text">Total Items: {totalQuantity}</h3>
+      <h3 className="cart-summary-text">Total Price: {totalPrice.toFixed(2)}</h3>
 
       <Link to="/cart" className="cart-link">
         Go to Cart
